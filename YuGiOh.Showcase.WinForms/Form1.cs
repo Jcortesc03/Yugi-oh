0ef51bf4ac0e2243a601615a7661c74f280a6632
juank ibarra <camibarras@gmail.com>
2025-12-02 18:23:56 -0500

    diseño

--- YuGiOh.Showcase.WinForms/Form1.cs
+++ YuGiOh.Showcase.WinForms/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace YuGiOh.Showcase.WinForms;
 
 public partial class Form1 : Form
@@ -5,5 +8,40 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void Btbocaarriba_Click(object sender, EventArgs e)
+    {
+        // Lógica para mostrar cartas boca arriba
+        MessageBox.Show("Mostrar cartas boca arriba");
+    }
+
+    private void BtnBocaabajo_Click(object sender, EventArgs e)
+    {
+        // Lógica para mostrar cartas boca abajo
+        MessageBox.Show("Mostrar cartas boca abajo");
+    }
+
+    private void BtnDetalles_Click(object sender, EventArgs e)
+    {
+        // Lógica para ver detalles de la carta seleccionada
+        MessageBox.Show("Mostrar detalles de la carta");
+    }
+
+    private void BtnJugar_Click(object sender, EventArgs e)
+    {
+        // Lógica para iniciar el juego
+        MessageBox.Show("¡Juego iniciado!");
+    }
+
+    private void carta4j2_Click(object sender, EventArgs e)
+    {
+        // Lógica cuando se hace clic en carta4j2
+        MessageBox.Show("Haz clic en carta4j2");
+    }
+
+    private void btnsalir_Click(object sender, EventArgs e)
+    {
+        this.Close();
     }
 }
---
+++ YuGiOh.Showcase.WinForms/carta.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace YuGiOh.Showcase.WinForms
+{
+    partial class carta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pbcartas = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pbcartas).BeginInit();
+            SuspendLayout();
+            // 
+            // pbcartas
+            // 
+            pbcartas.Location = new Point(-9, -9);
+            pbcartas.Name = "pbcartas";
+            pbcartas.Size = new Size(366, 510);
+            pbcartas.TabIndex = 0;
+            pbcartas.TabStop = false;
+            // 
+            // carta
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(347, 488);
+            ControlBox = false;
+            Controls.Add(pbcartas);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "carta";
+            ((System.ComponentModel.ISupportInitialize)pbcartas).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        public PictureBox pbcartas;
+    }
+}
