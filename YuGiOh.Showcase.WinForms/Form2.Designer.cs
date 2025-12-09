a67d9dc983b8ceb0733fe81a4a9d51abec9b86dd
Juan <Jcamilocortesc03@gmail.com>
2025-12-09 14:11:21 -0500

    Se hizo el primer merge

---
+++ YuGiOh.Showcase.WinForms/Form2.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace YuGiOh.Showcase.WinForms
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            videoView1 = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)videoView1).BeginInit();
+            SuspendLayout();
+            // 
+            // videoView1
+            // 
+            videoView1.BackColor = Color.Black;
+            videoView1.Dock = DockStyle.Fill;
+            videoView1.Location = new Point(0, 0);
+            videoView1.Margin = new Padding(4, 5, 4, 5);
+            videoView1.MediaPlayer = null;
+            videoView1.Name = "videoView1";
+            videoView1.Size = new Size(1067, 692);
+            videoView1.TabIndex = 0;
+            videoView1.Text = "videoView1";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1067, 692);
+            Controls.Add(videoView1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            FormClosing += Form2_FormClosing;
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)videoView1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+    }
+}
